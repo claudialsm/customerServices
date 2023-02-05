e725f311760080b4d6b479501fa44b5817f50396
claudialsm <68153639+claudialsm@users.noreply.github.com>
2023-02-05 00:38:29 +0000

    Add starter files

--- customers/src/main/java/com/checks/customer/CustomerApplication.java
+++ customers/src/main/java/com/checks/customer/CustomerApplication.java
@@ -1,13 +1,11 @@
-package com.check.customer;
+package com.checks.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CustomerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CustomerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerApplication.class, args);
+    }
 }
